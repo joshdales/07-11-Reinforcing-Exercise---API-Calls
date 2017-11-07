1d26fb81ac50072b9b60b200674127e2e16c0b34
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-07 09:38:24 -0500

    Display when and what the next elections are for

--- exercise.rb
+++ exercise.rb
@@ -4,10 +4,12 @@ toronto_wards_response = HTTParty.get('https://represent.opennorth.ca/boundaries
 
 toronto_wards_json = JSON.parse(toronto_wards_response.body)
 
-toronto_wards = ["objects"].map { |ward| ward["name"] }
+toronto_wards = toronto_wards_json["objects"].map { |ward| ward["name"] }
 
 puts toronto_wards
 
 elections_response = HTTParty.get('https://represent.opennorth.ca/elections/')
 
 elections_json = JSON.parse(elections_response.body)
+
+puts elections = elections_json["objects"].map { |election| "Election for #{election["name"]} on #{election["election_date"]}" }
