d09dc87ba099307d6803529e8696f920adc9f36c
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-07 09:16:44 -0500

    Parse json data

--- exercise.rb
+++ exercise.rb
@@ -1,3 +1,5 @@
 require 'httparty'
 
 toronto_wards_response = HTTParty.get('https://represent.opennorth.ca/boundaries/toronto-wards/')
+
+toronto_wards_json = JSON.parse(toronto_wards_response.body)
